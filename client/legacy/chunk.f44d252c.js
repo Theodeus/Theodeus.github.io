7aba6898c2f5b66516438d2842412c9f40e2b7cf
Oskar Eriksson <oskar.det@gmail.com>
2019-05-08 17:58:01 +0200

    Updated visuals and copy

---
+++ client/legacy/chunk.f44d252c.js
@@ -0,0 +1,2 @@
+import"./chunk.941586a1.js";function t(t,o){var a=o.delay,n=void 0===a?0:a,i=o.duration,r=void 0===i?400:i,c=+getComputedStyle(t).opacity;return{delay:n,duration:r,css:function(t){return"opacity: ".concat(t*c)}}}export{t as a};
+//# sourceMappingURL=chunk.f44d252c.js.map
