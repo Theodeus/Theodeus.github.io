8aad2eb29e3f66af81bdf84767ec9376326f5f19
theodeus <oskar.det@gmail.com>
2021-03-18 16:00:11 +0100

    Updated copy to reflect reality

---
+++ client/chunk.3e634f4b.js
@@ -0,0 +1,2 @@
+import"./chunk.4aa87d6d.js";function t(t,{delay:a=0,duration:o=400}){const d=+getComputedStyle(t).opacity;return{delay:a,duration:o,css:t=>`opacity: ${t*d}`}}export{t as a};
+//# sourceMappingURL=chunk.3e634f4b.js.map
---
+++ client/legacy/chunk.6e7e31e6.js
@@ -0,0 +1,2 @@
+import"./chunk.3d70bfe9.js";function t(t,o){var a=o.delay,n=void 0===a?0:a,e=o.duration,i=void 0===e?400:e,r=+getComputedStyle(t).opacity;return{delay:n,duration:i,css:function(t){return"opacity: ".concat(t*r)}}}export{t as a};
+//# sourceMappingURL=chunk.6e7e31e6.js.map
