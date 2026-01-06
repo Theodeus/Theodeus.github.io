7682edc2514d9de29d859ac9dea1e5ce0de2f098
Oskar <oskar.det@gmail.com>
2026-01-06 22:14:59 +0100

    Removed twitter and added bf6

--- client/chunk.dc4c6187.js
+++
@@ -1,2 +0,0 @@
-import"./chunk.e7473d5b.js";function t(t,{delay:o=0,duration:a=400}){const e=+getComputedStyle(t).opacity;return{delay:o,duration:a,css:t=>`opacity: ${t*e}`}}export{t as a};
-//# sourceMappingURL=chunk.dc4c6187.js.map
